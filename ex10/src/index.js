c5687022449f06210129eca6cea7c2be71ee9522
aanc1122 <amelanuhanovic997@gmail.com>
2021-03-11 13:22:41 +0100

    Declare String Variables

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+
+    var myFirstName = "Amela";
+    var myLastName = "Nuhanović";
+    var myHomeTown = "Konjic";
+
+    return {
+
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.export = main;
